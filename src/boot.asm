4efe1c0d6d12436853b4d645d827b54c6bbe5855
yichuan-acc <pympengyimeng@126.com>
2023-02-12 05:59:30 +0800

    b6 finish

--- src/boot.asm
+++ src/boot.asm
@@ -11,13 +11,164 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-xchg bx,bx
 
 mov si,booting
 call print
+xchg bx,bx
+
+mov edi, 0x1000; 读取的目标内存
+mov ecx, 0; 起始扇区
+mov bl, 1; 扇区数量
+
+call read_disk
+xchg bx,bx
+
+mov edi, 0x1000; 写入的目标内存
+mov ecx, 2; 起始扇区
+mov bl, 1; 扇区数量
+
+call write_disk
+xchg bx,bx
+
 
 ;阻塞
 jmp $
+
+read_disk:
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx; 0x1f3
+    mov al, cl; 起始扇区的前八位
+    out dx, al
+
+    inc dx; 0x1f4
+    shr ecx, 8
+    mov al, cl; 起始扇区的中八位
+    out dx, al
+
+    inc dx; 0x1f5
+    shr ecx, 8
+    mov al, cl; 起始扇区的高八位
+    out dx, al
+
+    inc dx; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111; 将高四位置为 0
+
+    mov al, 0b1110_0000;
+    or al, cl
+    out dx, al; 主盘 - LBA 模式
+
+    inc dx; 0x1f7
+    mov al, 0x20; 读硬盘
+    out dx, al
+
+    xor ecx, ecx; 将 ecx 清空
+    mov cl, bl; 得到读写扇区的数量
+
+    .read:
+        push cx;保存cx
+        call .waits; 等待数据准备完毕
+        call .reads; 读取一个扇区
+        pop cx;恢复cx
+        loop .read
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2; nop 直接跳转到下一行
+            jmp $+2; 一点点延迟
+            jmp $+2
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000
+            jnz .check
+        ret
+    
+    .reads:
+        mov dx, 0x1f0
+        mov cx, 256; 一个扇区 256 字
+        .readw:
+            in ax, dx
+            jmp $+2; 一点点延迟
+            jmp $+2
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret
+
+write_disk:
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx; 0x1f3
+    mov al, cl; 起始扇区的前八位
+    out dx, al
+
+    inc dx; 0x1f4
+    shr ecx, 8
+    mov al, cl; 起始扇区的中八位
+    out dx, al
+
+    inc dx; 0x1f5
+    shr ecx, 8
+    mov al, cl; 起始扇区的高八位
+    out dx, al
+
+    inc dx; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111; 将高四位置为 0
+
+    mov al, 0b1110_0000;
+    or al, cl
+    out dx, al; 主盘 - LBA 模式
+
+    inc dx; 0x1f7
+    mov al, 0x30; 写硬盘,在此处设置系统调用接口
+    out dx, al
+
+    xor ecx, ecx; 将 ecx 清空
+    mov cl, bl; 得到读写扇区的数量
+
+    .write:
+        push cx;保存cx
+        call .writes; 写入一个扇区
+        call .waits; 等待数据写入完毕
+        pop cx;恢复cx
+        loop .write
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2; nop 直接跳转到下一行
+            jmp $+2; 一点点延迟
+            jmp $+2
+            and al, 0b1000_0000
+            cmp al, 0b0000_0000
+            jnz .check
+        ret
+    
+    .writes:
+        mov dx, 0x1f0
+        mov cx, 256; 一个扇区 256 字
+        .writew:
+            mov  ax,[edi]
+            out dx, ax
+            jmp $+2; 一点点延迟
+            jmp $+2
+            jmp $+2
+            add edi, 2
+            loop .writew
+        ret
 
 print:
     mov ah, 0x0e
@@ -31,9 +182,15 @@ print:
 .done:
     ret
 
-
 booting:
     db "Booting Onix...",10,13,0; \n\r
+
+error:
+    mov si, .msg
+    call print
+    hlt; 让 CPU 停止
+    jmp $
+    .msg db "Booting Error!!!", 10, 13, 0
 
 ; 填充 0
 times 510 - ($ - $$) db 0
