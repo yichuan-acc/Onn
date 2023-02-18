08dceeb5aca7d5284027c15d5034875daf2cf5cc
yichuan-acc <pympengyimeng@126.com>
2023-02-18 17:05:00 +0800

    b11 finish | 讲述了elf文件

---
+++ src/test/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+char message[]="hello world\n";
+char buf[1024];
+
+int main(){
+
+    printf(message);
+
+    return 0;
+}
---
+++ src/test/readme.md
@@ -0,0 +1,11 @@
+# 
+
+gcc -m32 hello.c -o hello
+32位编译
+
+-static
+---
+
+readelf -e hello
+读取elf文件
+
