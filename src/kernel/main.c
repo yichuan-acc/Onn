f04926e4696187618ac3b7872d545faa67775743
yichuan-acc <pympengyimeng@126.com>
2023-02-19 08:02:53 +0800

    b20 finish

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,14 +1,28 @@
 #include <onix/onix.h>
+#include <onix/types.h>
+#include <onix/io.h>
 
-int magic = ONIX_MAGIC;
-char message[] = "hello onix!!!"; //.data
-char buf[1024];                   //.bss
+#define CRT_ADDR_REG 0X3d4
+#define CRT_DATA_REG 0X3d5
+
+#define CRT_CURSOR_H 0Xe
+#define CRT_CURSOR_L 0Xf
 
 void kernel_init()
 {
-    char *video = (char *)0xb8000; // 文本显示器的内存位置
-    for (int i = 0; i < sizeof(message); i++)
-    {
-        video[i * 2] = message[i];
-    }
+
+    outb(CRT_ADDR_REG, CRT_CURSOR_H);
+    u16 pos = inb(CRT_DATA_REG) << 8;
+
+    outb(CRT_ADDR_REG, CRT_CURSOR_L);
+
+    pos |= inb(CRT_DATA_REG);
+    // u8 data = inb(CRT_DATA_REG);
+
+    outb(CRT_ADDR_REG, CRT_CURSOR_H);
+    outb(CRT_DATA_REG, 0);
+    outb(CRT_ADDR_REG, CRT_CURSOR_L);
+    outb(CRT_DATA_REG, 123);
+
+    return;
 }
